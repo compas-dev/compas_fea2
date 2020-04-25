a8062f2224b86a2b41411ba352c1ac927c8d252e
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2020-04-25 16:25:17 +0200

    writer moved to main level

--- src/compas_fea2/backends/abaqus/writer/writer.py
+++ src/compas_fea2/backends/abaqus/writer/writer.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from compas_fea2.utilities import cWriter
+
 from compas_fea2.backends.abaqus.writer import Heading
 from compas_fea2.backends.abaqus.writer.elements import Elements
 from compas_fea2.backends.abaqus.writer.elements import Nodes
@@ -19,11 +21,9 @@ __all__ = [
     'Writer',
 ]
 
+class Writer(cWriter, Steps, Materials, BCs, Elements, Nodes, Sets, Heading):
 
-
-class Writer(Steps, Materials, BCs, Sets, Elements, Nodes, Heading):
-
-    """ Initialises base file writer.
+    """ Initialises abaqus file writer.
 
     Parameters
     ----------
@@ -36,50 +36,6 @@ class Writer(Steps, Materials, BCs, Sets, Elements, Nodes, Heading):
     """
 
     def __init__(self, structure, filename, fields, ndof=6):
-
+        super(Writer, self).__init__(structure, filename, fields, ndof)
         self.comment   = '**'
-        self.filename  = filename
-        self.ndof      = ndof
-        self.structure = structure
-        self.fields    = fields
         self.spacer    = ', '
-
-
-    def __enter__(self):
-
-        self.file = open(self.filename, 'w')
-        return self
-
-
-    def __exit__(self, type, value, traceback):
-
-        self.file.close()
-
-
-    def blank_line(self):
-
-        self.file.write('{0}\n'.format(self.comment))
-
-
-    def divider_line(self):
-
-        self.file.write('{0}------------------------------------------------------------------\n'.format(self.comment))
-
-
-    def write_line(self, line):
-
-        self.file.write('{0}\n'.format(line))
-
-
-    def write_section(self, section):
-
-        self.divider_line()
-        self.write_line('{0} {1}'.format(self.comment, section))
-        self.divider_line()
-
-
-    def write_subsection(self, subsection):
-
-        self.write_line('{0} {1}'.format(self.comment, subsection))
-        self.write_line('{0}-{1}'.format(self.comment, '-' * len(subsection)))
-        self.blank_line()
--- src/compas_fea2/backends/opensees/writer/writer.py
+++ src/compas_fea2/backends/opensees/writer/writer.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from compas_fea2.utilities import cWriter
+
 from compas_fea2.backends.opensees.writer import Heading
 from compas_fea2.backends.opensees.writer.elements import Elements
 from compas_fea2.backends.opensees.writer.elements import Nodes
@@ -18,8 +20,7 @@ __all__ = [
     'Writer',
 ]
 
-
-class Writer(Steps, Materials, BCs, Elements, Nodes, Heading):
+class Writer(cWriter, Steps, Materials, BCs, Elements, Nodes, Heading):
 
     """ Initialises base file writer.
 
@@ -34,51 +35,6 @@ class Writer(Steps, Materials, BCs, Elements, Nodes, Heading):
     """
 
     def __init__(self, structure, filename, fields, ndof=6):
-
-        self.comment   = '#' #TODO I can bring the writer in utilities and just change these characters
-        self.filename  = filename
-        self.ndof      = ndof
-        self.structure = structure
-        self.fields    = fields
+        super(Writer, self).__init__(structure, filename, fields, ndof)
+        self.comment   = '#'
         self.spacer    = ' '
-
-        #TODO check if it is possible to collecte all the tags here
-
-    def __enter__(self):
-
-        self.file = open(self.filename, 'w')
-        return self
-
-
-    def __exit__(self, type, value, traceback):
-
-        self.file.close()
-
-
-    def blank_line(self):
-
-        self.file.write('{0}\n'.format(self.comment))
-
-
-    def divider_line(self):
-
-        self.file.write('{0}------------------------------------------------------------------\n'.format(self.comment))
-
-
-    def write_line(self, line):
-
-        self.file.write('{0}\n'.format(line))
-
-
-    def write_section(self, section):
-
-        self.divider_line()
-        self.write_line('{0} {1}'.format(self.comment, section))
-        self.divider_line()
-
-
-    def write_subsection(self, subsection):
-
-        self.write_line('{0} {1}'.format(self.comment, subsection))
-        self.write_line('{0}-{1}'.format(self.comment, '-' * len(subsection)))
-        self.blank_line()

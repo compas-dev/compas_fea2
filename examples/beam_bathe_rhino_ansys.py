f92360aa1b3635289f29c90030b7733431703e1b
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2020-05-19 18:11:29 +0200

    tested with ansys and added some examples

---
+++ examples/beam_bathe_rhino_ansys.py
@@ -0,0 +1,81 @@
+
+from compas.geometry import cross_vectors
+from compas.geometry import normalize_vector
+from compas.geometry import subtract_vectors
+
+from compas_fea2.cad import rhino
+from compas_fea2.backends.ansys.core import FixedDisplacement
+from compas_fea2.backends.ansys.core import ElasticIsotropic
+from compas_fea2.backends.ansys.core import ElementProperties as Properties
+from compas_fea2.backends.ansys.core import FixedDisplacement
+from compas_fea2.backends.ansys.core import GeneralStep
+from compas_fea2.backends.ansys.core import PointLoad
+from compas_fea2.backends.ansys.core import RectangularSection
+from compas_fea2.backends.ansys.core import Structure
+
+import rhinoscriptsyntax as rs
+import json
+
+
+# Author(s): Andrew Liew (github.com/andrewliew)
+
+
+# Local ex
+
+for i in rs.ObjectsByLayer('elset_beams'):
+    ez = subtract_vectors(rs.CurveEndPoint(i), rs.CurveStartPoint(i))
+    ex = normalize_vector(cross_vectors(ez, [0, 0, 1]))
+    rs.ObjectName(i, '_{0}'.format(json.dumps({'ex': ex})))
+
+# Structure
+
+mdl = Structure(name='beam_bathe', path='C:/Temp/')
+
+# Elements
+
+rhino.add_nodes_elements_from_layers(mdl, line_type='BeamElement', layers='elset_beams')
+
+# Sets
+
+rhino.add_sets_from_layers(mdl, layers=['nset_support', 'nset_load'])
+
+# Materials
+
+mdl.add(ElasticIsotropic(name='mat_elastic', E=10**7, v=10**(-5), p=1))
+
+# Sections
+
+mdl.add(RectangularSection(name='sec_beam', b=1, h=1))
+
+# Properties
+
+mdl.add(Properties(name='ep_beam', material='mat_elastic', section='sec_beam', elset='elset_beams'))
+
+# Displacements
+
+mdl.add(FixedDisplacement(name='disp_fixed', nodes='nset_support'))
+
+# Loads
+
+mdl.add(PointLoad(name='load_point', nodes='nset_load', z=600))
+
+# Steps
+
+mdl.add([
+    GeneralStep(name='step_bc', displacements=['disp_fixed']),
+    GeneralStep(name='step_load', loads=['load_point']),
+])
+mdl.steps_order = ['step_bc', 'step_load']
+
+# Summary
+
+mdl.summary()
+
+
+# Run
+
+mdl.analyse_and_extract(fields=['u', 'sf', 'sm'], license='teaching')
+
+rhino.plot_data(mdl, step='step_load', field='uz', radius=1)
+#
+#print(mdl.get_nodal_results(step='step_load', field='uz', nodes='nset_load'))
---
+++ examples/beam_frame_rhino_ansys.py
@@ -0,0 +1,78 @@
+
+from compas_fea2.cad import rhino
+from compas_fea2.backends.ansys.core import ElementProperties as Properties
+from compas_fea2.backends.ansys.core import GeneralStep
+from compas_fea2.backends.ansys.core import PinnedDisplacement
+from compas_fea2.backends.ansys.core import PipeSection
+from compas_fea2.backends.ansys.core import PointLoad
+from compas_fea2.backends.ansys.core import RollerDisplacementXZ
+from compas_fea2.backends.ansys.core import Steel
+from compas_fea2.backends.ansys.core import Structure
+
+
+# Author(s): Andrew Liew (github.com/andrewliew)
+
+
+# Structure
+
+mdl = Structure(name='beam_frame', path='C:/Temp/')
+
+# Elements
+
+rhino.add_nodes_elements_from_layers(mdl, line_type='BeamElement', layers='elset_beams')
+
+# Sets
+
+rhino.add_sets_from_layers(mdl, layers=['nset_pins', 'nset_load_v', 'nset_load_h', 'nset_rollers'])
+
+# Materials
+
+mdl.add(Steel(name='mat_steel'))
+
+# Sections
+
+mdl.add(PipeSection(name='sec_pipe', r=0.100, t=0.005))
+
+# Properties
+
+mdl.add(Properties(name='ep_beam', material='mat_steel', section='sec_pipe', elset='elset_beams'))
+
+# Displacements
+
+mdl.add([
+    PinnedDisplacement(name='disp_pins', nodes='nset_pins'),
+    RollerDisplacementXZ(name='disp_rollers', nodes='nset_rollers'),
+])
+
+# Loads
+
+mdl.add([
+    PointLoad(name='load_h', nodes='nset_load_h', x=+4000),
+    PointLoad(name='load_v', nodes='nset_load_v', z=-6000),
+])
+
+# Steps
+
+mdl.add([
+    GeneralStep(name='step_bc', displacements=['disp_pins', 'disp_rollers']),
+    GeneralStep(name='step_loads', loads=['load_h', 'load_v'], iterations=50),
+])
+mdl.steps_order = ['step_bc', 'step_loads']
+
+# Summary
+
+mdl.summary()
+
+# Run
+
+mdl.analyse_and_extract(fields=['u', 'rf', 'sf', 'sm'], license='teaching')
+
+rhino.plot_data(mdl, step='step_loads', field='um', scale=50)
+rhino.plot_data(mdl, step='step_loads', field='sf1')
+rhino.plot_data(mdl, step='step_loads', field='sf2')
+rhino.plot_data(mdl, step='step_loads', field='sm1')
+rhino.plot_reaction_forces(mdl, step='step_loads', scale=0.5)
+
+print(mdl.get_nodal_results(step='step_loads', field='rfm', nodes='nset_pins'))
+print(mdl.get_nodal_results(step='step_loads', field='um', nodes='nset_load_v'))
+

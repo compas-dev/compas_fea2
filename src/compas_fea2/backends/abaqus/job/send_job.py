a6741233ba7e2b81fb41518f4ff95e8c08be335e
frankie <ranaudo@arch.ethz.ch>
2021-03-13 23:52:05 +0100

    optimisation job working

--- src/compas_fea2/backends/abaqus/job/send_job.py
+++ src/compas_fea2/backends/abaqus/job/send_job.py
@@ -15,20 +15,15 @@ __all__ = [
     'launch_process',
 ]
 
-def launch_process(structure, path, exe, cpus, output, overwrite, user_mat):
-
-    """ Runs the analysis through Abaqus.
+def launch_process(problem, exe, output, overwrite, user_mat):
+    """ Run the analysis through Abaqus.
 
     Parameters
     ----------
-    structure : obj
-        Structure object.
-    path : str
-        Path where to start the process.
+    problem : obj
+        problem object.
     exe : str
         Abaqus exe path to bypass defaults.
-    cpus : int
-        Number of CPU cores to use.
     output : bool
         Print terminal output.
     overwrite : bool
@@ -49,7 +44,7 @@ def launch_process(structure, path, exe, cpus, output, overwrite, user_mat):
     if overwrite:
         overwrite_kw = 'ask_delete=OFF'
     if user_mat:
-        umat_path = structure.materials[user_mat].sub_path
+        umat_path = problem.materials[user_mat].sub_path
         user_sub_kw ='user={}'.format(umat_path)
     if exe:
         exe_kw = exe
@@ -57,8 +52,8 @@ def launch_process(structure, path, exe, cpus, output, overwrite, user_mat):
     # Analyse
     tic = time()
     success    = False
-    cmd='cd {} && {} {} job={} interactive {}'.format(path, exe_kw, user_sub_kw, structure.name, overwrite_kw)
-    p    = Popen(cmd, stdout=PIPE, stderr=PIPE, cwd=path, shell=True)
+    cmd='cd {} && {} {} job={} interactive {}'.format(problem.path, exe_kw, user_sub_kw, problem.name, overwrite_kw)
+    p    = Popen(cmd, stdout=PIPE, stderr=PIPE, cwd=problem.path, shell=True)
 
     while True:
         line = p.stdout.readline()
@@ -82,7 +77,7 @@ def launch_process(structure, path, exe, cpus, output, overwrite, user_mat):
 
     if not success:
         try:
-            with open(path + structure.name + '.sta', 'r') as f:
+            with open(problem.path + problem.name + '.sta', 'r') as f:
                 if 'COMPLETED SUCCESSFULLY' in f.readlines()[-1]:
                     success = True
         except:
@@ -95,4 +90,61 @@ def launch_process(structure, path, exe, cpus, output, overwrite, user_mat):
         print('***** Analysis failed *****')
 
 
+def launch_optimisation(problem, output):
+    """ Run the topology optimisation through Tosca.
 
+    Parameters
+    ----------
+    problem : obj
+        Problem object.
+    output : bool
+        Print terminal output.
+
+    Returns
+    -------
+    None
+
+    """
+
+    # Set options
+    exe_kw='ToscaStructure'
+
+    # Analyse
+    tic = time()
+    success    = False
+    cmd='cd {} && {} --job {} --cpus {}'.format(problem.path, exe_kw, problem.name, problem.cpus)
+    p    = Popen(cmd, stdout=PIPE, stderr=PIPE, cwd=problem.path, shell=True)
+
+    while True:
+        line = p.stdout.readline()
+        if not line:
+            break
+        line = str(line.strip())
+
+        if output:
+            print(line)
+
+        if 'Application finished successfully' in line:
+            success = True
+
+    stdout, stderr = p.communicate()
+
+    if output:
+        print(stdout)
+        print(stderr)
+
+    toc = time() - tic
+
+    if not success:
+        try:
+            with open(problem.path + problem.name + '.sta', 'r') as f:
+                if 'COMPLETED SUCCESSFULLY' in f.readlines()[-1]:
+                    success = True
+        except:
+            pass
+
+    if success:
+        if output:
+            print('***** Analysis successful - analysis time : {0} s *****'.format(toc))
+    else:
+        print('***** Analysis failed *****')

ad775b0f2121f65f66dad81f6fb6123353017ca3
frankie <ranaudo@arch.ethz.ch>
2021-03-14 15:15:57 +0100

    gmsh non-planar faces

--- examples/WIP/gmesh_to_compas.py
+++ examples/WIP/gmesh_to_compas.py
@@ -37,46 +37,51 @@ def gmesh_to_compas(gmshModel):
                 n = gmshModel.mesh.getElementProperties(etype)[3]
                 triangle = ntags[i * n: i * n + n]
                 triangles.append(triangle.tolist())
+    # gmshModel.finalize()
     return Mesh.from_vertices_and_faces(xyz, triangles)
 
-def gmsh_geometry(lc = 50):
+def compas_to_gmsh(mesh, lc=100, write_path=None, inspect=False):
     gmsh.initialize(sys.argv)
-    gmsh.model.add("t1")
+    gmsh.model.add("mesh")
 
-    gmsh.model.geo.addPoint(0, 0, 0, lc, 1)
-    gmsh.model.geo.addPoint(1000, 0, 0, lc, 2)
-    gmsh.model.geo.addPoint(1000, 3000, 0, lc, 3)
-    p4 = gmsh.model.geo.addPoint(0, 3000, 0, lc)
-    gmsh.model.geo.addLine(1, 2, 1)
-    gmsh.model.geo.addLine(3, 2, 2)
-    gmsh.model.geo.addLine(3, p4, 3)
-    gmsh.model.geo.addLine(4, 1, p4)
-    gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
-    gmsh.model.geo.addPlaneSurface([1], 1)
+    for v in mesh.vertices():
+        gmsh.model.geo.addPoint(*mesh.vertex_coordinates(v), lc, v)
 
+    # Generate elements between nodes
+    key_index = mesh.key_index()
+    faces = [[key_index[key]
+                for key in mesh.face_vertices(face)] for face in mesh.faces()]
+
+    c=1
+    for n, f in enumerate(faces):
+        n=n+1
+        for i in range(len(f)):
+            p1 = f[i]
+            p2 = f[(i + 1) % len(f)]
+            gmsh.model.geo.addLine(p1, p2, c)
+            c+=1
+        gmsh.model.geo.addCurveLoop([c-4, c-3, c-2, c-1], n)
+        # gmsh.model.geo.addPlaneSurface([n], n)
+        gmsh.model.geo.addSurfaceFilling([n], n)
     gmsh.model.geo.synchronize()
-    gmsh.model.addPhysicalGroup(1, [1, 2, 4], 5)
-    ps = gmsh.model.addPhysicalGroup(2, [1])
-    gmsh.model.setPhysicalName(2, ps, "My surface")
-
-    # We can then generate a 2D mesh...
+    gmsh.model.mesh.refine()
     gmsh.model.mesh.generate(2)
-    print(gmsh.model.mesh.getNode(nodeTag=10))
-    element = gmsh.model.mesh.getElement(10)
-    # gmsh.model.mesh.getElementFaceNodes()
-    # print(gmsh.model.mesh.getKeysForElement(1,'Lagrange'))
+    if inspect:
+        gmsh.fltk.run()
+    if write_path:
+        gmsh.write("{}/t1.stl".format(write_path))
+    return gmsh.model
 
-    # ... and save it to disk
-    gmsh.write(DATA+"/t1.stl")
-    mesh = gmesh_to_compas(gmsh.model)
-    gmsh.finalize()
-    return mesh
 
-mesh = gmsh_geometry()
+# Get a Mesh geometry to create the model
+mesh = Mesh.from_obj(DATA + '/hypar.obj')
+gmesh = compas_to_gmsh(mesh, write_path=TEMP)
+
+mesh = gmesh_to_compas(gmshModel=gmesh)
 
 ##### ----------------------------- MODEL ----------------------------- #####
 # Initialise the assembly object
-model = Model(name='cantilever_gmsh')
+model = Model(name='hypar_gmsh')
 
 # Define materials
 model.add_material(ElasticIsotropic(name='mat_A', E=29000, v=0.17, p=2.5e-9))
@@ -88,9 +93,9 @@ shell_20 = ShellSection(name='section_A', material='mat_A', t=20)
 model.shell_from_mesh(mesh=mesh, shell_section=shell_20)
 
 # Find nodes in the model for the boundary conditions
-n_fixed = model.get_node_from_coordinates([0, 0, 0,], 1)
-n_roller  = model.get_node_from_coordinates([1000, 0, 0], 1)
-n_load = model.get_node_from_coordinates([1000, 3000, 0,], 1)
+n_fixed = model.get_node_from_coordinates([5000, 0, 0,], 10)
+n_roller  = model.get_node_from_coordinates([0, 0, -5000], 10)
+n_load = model.get_node_from_coordinates([0, 3000, 0,], 10)
 
 # Define sets for boundary conditions and loads
 model.add_assembly_set(Set(name='fixed', selection=[n_fixed['part-1']], stype='nset'), instance='part-1-1')
@@ -102,7 +107,7 @@ model.summary()
 ##### ----------------------------- PROBLEM ----------------------------- #####
 
 # Create the Problem object
-problem = Problem(name='cantilever_gmsh', model=model)
+problem = Problem(name='hypar_gmsh', model=model)
 
 # Assign boundary conditions to the node stes
 problem.add_bcs(bcs=[RollerDisplacementXZ(name='bc_roller', bset='roller'),
@@ -117,7 +122,7 @@ problem.add_step(step=GeneralStaticStep(name='gstep', loads=['pload']))
 problem.summary()
 
 # Solve the problem
-problem.analyse(path= TEMP + '/cantilever_gmsh')
+problem.analyse(path= TEMP + '/hypar_gmsh')
 problem.extract()
 
 print(problem.results)

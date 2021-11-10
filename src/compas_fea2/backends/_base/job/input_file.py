b1c0e3b590c45ec6037da80850e2325748957634
frankie <ranaudo@arch.ethz.ch>
2021-11-10 15:12:47 +0100

    inputfile _base

---
+++ src/compas_fea2/backends/_base/job/input_file.py
@@ -0,0 +1,47 @@
+import os
+
+
+class InputFileBase():
+    """Input file object for standard analysis.
+
+    Parameters
+    ----------
+    problem : obj
+        Problem object.
+
+    Attributes
+    ----------
+    job_name : str
+        Name of the Abaqus job. This is the same as the input file name.
+    """
+
+    def __init__(self, problem):
+        self.job_name = problem.name
+
+    # ==============================================================================
+    # General methods
+    # ==============================================================================
+
+    def write_to_file(self, path):
+        """Writes the InputFile to a file in a specified location.
+
+        Parameters
+        ----------
+        path : str
+            Path to the folder where the input file will be saved.
+
+        Returns
+        -------
+        r : str
+            Information about the results of the writing process.
+        """
+
+        try:
+            filepath = os.path.join(path, self.name)
+            with open(filepath, 'w') as f:
+                f.writelines(self.data)
+            r = '***** {} generated in: {} *****\n'.format(self._input_file_type, filepath)
+        except:
+            r = '***** ERROR: Input file not generated ****'
+
+        return r

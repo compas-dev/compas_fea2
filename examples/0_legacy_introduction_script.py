dc4d5d736ec7cccd85275de20d79cc7bbb5e4ea4
frankie <ranaudo@arch.ethz.ch>
2021-12-15 18:59:03 +0100

    example cleaned up

--- examples/0_legacy_introduction_script.py
+++ examples/0_legacy_introduction_script.py
@@ -39,53 +39,44 @@ model.add_material(ElasticIsotropic(name='mat_elastic', E=10*10**9, v=0.3, p=150
 
 # Define sections
 model.add_section(CircularSection(name='sec_circ',    material='mat_elastic', r=0.010))
-# model.add_section(ShellSection(name='sec_shell',   material='mat_elastic', t=0.005))
 
 # Generate elements between nodes
 connectivity = [[0, i] for i in range(1, 5)]
 model.add_elements([BeamElement(connectivity=conn, section='sec_circ') for conn in connectivity], part='part-1')
-# model.add_element(ShellElement(connectivity=[0, 1, 4], section='sec_shell'), part='part-1')
 
-# Define sets for boundary conditions and loads
-model.add_nodes_group(name='nset_base', nodes=[1, 2, 3, 4], part='part-1')
-model.add_nodes_group(name='nset_top', nodes=[0], part='part-1')
-model.add_elements_group(name='elset_beams',  elements=[0, 1, 2, 3], part='part-1')
-# Assign boundary conditions
+# Assign boundary conditions (3 pins and a rollerXY)
 model.add_pin_bc(name='bc_pinned', part='part-1', nodes=[1, 2, 3])
 model.add_rollerXY_bc(name='bc_roller', part='part-1', nodes=[4])
-# model.summary()
-# model.show()
+
+# Review
+model.summary()
+model.show()
 
 
 ##### ----------------------------- PROBLEM ----------------------------- #####
 # Create the Problem object
 problem = Problem(name='test_structure', model=model)
-# Assign a point load to the node set
-# step_0 = GeneralStaticStep(name='step_gravity')
+
+# Approach 1: Create a step and assign a gravity load
 step_0 = GeneralStaticStep(name='step_pload_0')
 step_0.add_gravity_load()
 problem.add_step(step_0)
 
+# Approach 2: Add a step and define a point load directly from Problem
 problem.add_step(GeneralStaticStep(name='step_pload_1'))
 problem.add_point_load(name='load_point', x=10000, z=-10000, nodes=[0], part='part-1', step='step_pload_1')
-# problem.add_displacements([GeneralDisplacement('disp_pinned', 'nset_a', x=0, y=0, z=-0.05), pin], step_1)
 
 # Define the field outputs required
 fout = FieldOutput(name='fout')
 problem.add_output(fout, step_0)
-# problem.add_output(fout, step_1)
 
-# Define the analysis step (there should be a message skipping the step, since they were already added)
-# problem.add_steps([step_0, step_1])
-
-# problem.summary()
-# problem.show()
+# Review
+problem.summary()
+problem.show()
 
 # Solve the problem
-# problem.path = Path(TEMP).joinpath(problem.name)
-# problem.write_input_file()
 problem.analyse(path=Path(TEMP).joinpath(problem.name))
 
-# # ##### --------------------- POSTPROCESS RESULTS -------------------------- #####
-# results = Results.from_problem(problem, fields=['u'])
-# pprint(results.nodal)
+##### --------------------- POSTPROCESS RESULTS -------------------------- #####
+results = Results.from_problem(problem, fields=['u'])
+pprint(results.nodal)

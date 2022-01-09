c5f4bbb396e8e7f3a3027f90ab7ac8ff41afd3b9
frankie <ranaudo@arch.ethz.ch>
2022-01-09 15:41:34 +0100

    contact pair

--- src/compas_fea2/backends/_base/model/contacts.py
+++ src/compas_fea2/backends/_base/model/contacts.py
@@ -2,8 +2,37 @@ from compas_fea2.backends._base.base import FEABase
 
 
 class ContactPairBase(FEABase):
+    """Pair of master and slave surfaces to assign an interaction property
+
+    Parameters
+    ----------
+    FEABase : [type]
+        [description]
+    """
+
     def __init__(self, name, master, slave, interaction):
         self._name = name
         self._master = master
         self._slave = slave
         self._interaction = interaction
+
+    @property
+    def name(self):
+        """str : the name of the contact pair."""
+        return self._name
+
+    @property
+    def master(self):
+        """obj : type :class:`SurfaceBase` object to be used as master."""
+        return self._master
+
+    @property
+    def slave(self):
+        """obj : type :class:`SurfaceBase` object to be used as slave."""
+        return self._slave
+
+    @property
+    def interaction(self):
+        """str : name of a previusly defined :class:`InterfaceBase` object to
+        define the type of interaction between master and slave."""
+        return self._interaction

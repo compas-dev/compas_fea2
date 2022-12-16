b533180d88385a8c5aab951be1fc0346d390dba3
gab96mat <mattei@dbt-ethz.ch>
2022-12-16 18:43:46 +0100

    FIX_InputFile

--- src/compas_fea2/backends/sofistik/job/input_file.py
+++ src/compas_fea2/backends/sofistik/job/input_file.py
@@ -5,13 +5,6 @@ from __future__ import print_function
 from compas_fea2.job.input_file import InputFile
 from compas_fea2.job.input_file import ParametersFile
 
-# --------------------------- READ ME ------------------------------------------- 
-# This is the only part where I have this kind of comments.
-# TO FIX:
-# >>> in loads.py:
-# >>> >>> line loadcase_data_section = ["LC {} TITL 'point load'".format(i+1) for i in range(len(nodes))]
-# >>> >>> problem: need to get the indeces of the Load Cases LC.
-
 class SofistikInputFile(InputFile):
     """Sofistik implementation of :class:`compas_fea2.job.input_file.InputFile`.\n
     """
@@ -36,14 +29,13 @@ class SofistikInputFile(InputFile):
         """
 
         
-        return """$
+        return """
 $ MODEL
 {}
 $ PROBLEM
 {}
 """.format(self.model._generate_jobdata(),
-self.problem._generate_jobdata())
-
+           self.problem._generate_jobdata())
 
 
 class SofistikParametersFile(ParametersFile):

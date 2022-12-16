2b5a412c0db0da38ea9a428e669d3f41b514fcab
gab96mat <mattei@dbt-ethz.ch>
2022-12-16 18:27:49 +0100

    FIX_Nodes

--- src/compas_fea2/backends/sofistik/model/nodes.py
+++ src/compas_fea2/backends/sofistik/model/nodes.py
@@ -6,6 +6,12 @@ from compas_fea2.model.nodes import Node
 
 class SofistikNode(Node):
     """Sofistik implementation of :class:`compas_fea2.model.nodes.Node`.\n
+    
+    Note
+    ----
+    The nodes key numbering in compas_fea2 starts from 0, while in Abaqus it starts
+    from 1. The conversion in automatically resolved by compas_fea2.
+    
     """
     __doc__ += Node.__doc__
 
@@ -13,5 +19,11 @@ class SofistikNode(Node):
         super(SofistikNode, self).__init__(xyz=xyz, mass=mass, name=name, **kwargs)
 
     def _generate_jobdata(self):
-        return """NODE NO {} X {} Y {} Z {}""".format(self.key+1, self.x, self.y, self.z)
+        """Generates the common string information for the input file of the command
+        'NODE - Nodes, Coordinates and Constraints' defined in the SOFiSTiK programme module SOFiMSHA."""
+
+        return """NODE NO {} X {} Y {} Z {}""".format(self.key+1,
+                                                      self.x,
+                                                      self.y,
+                                                      self.z)
 
